e08611747b0dd19a74b450a6437a3f2e4b9b4aaa
Nevrik-core <57769617+Nevrik-core@users.noreply.github.com>
2022-07-22 14:51:59 +0300

    Task 9 complete

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonEl = document.querySelector('.change-color');
+const bgSpanEl = buttonEl.previousElementSibling.lastChild;
+
+buttonEl.addEventListener('click', () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  bgSpanEl.textContent = document.body.style.backgroundColor;
+});
+
+
+
