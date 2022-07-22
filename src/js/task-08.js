2c9813d07b16dd97f2d0448a173507b4fb1b9ed9
Nevrik-core <57769617+Nevrik-core@users.noreply.github.com>
2022-07-22 14:07:18 +0300

    Task 8 complete

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,19 @@
 
+const formEl = document.querySelector('.login-form');
+
+formEl.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    console.log(event.currentTarget.elements);
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+    }
+    
+    console.log(`Login: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
